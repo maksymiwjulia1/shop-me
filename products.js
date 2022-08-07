f0ccfe8938d434daa03b170bebaaf8da23613079
maksymiwjulia1 <93843187+maksymiwjulia1@users.noreply.github.com>
2022-08-07 05:17:24 -0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Гамбургер буряковий з курячим битком 280г",
+    desc : "власне виробництво",
+    img : "dummy-pdt-a.webp",
+    price : 100
+  },
+  124: {
+    name : "Кебаб в лаваші",
+    desc : "Оновлений рецепт ",
+    img : "dummy-pdt-b.jpg",
+    price : 80
+  },
+  125: {
+    name : "Картопля фрі",
+    desc : "Смачно як у макдональсі",
+    img : "dummy-pdt-c.webp",
+    price : 60
+  },
+  126: {
+    name : "Домашній хот-дог в булочці",
+    desc : "Приготовлено як у дома",
+    img : "dummy-pdt-d.webp",
+    price : 50
+  }
+};
